0f251db822ee7a64a034efabf886fbd626a57292
vbmakarov <vbmakarov.info@gmail.com>
2022-06-25 11:42:21 +0300

    bicycle

---
+++ bicycle/js/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+		var w=$(window).outerWidth();
+		var h=$(window).outerHeight();
+		var ua = window.navigator.userAgent;
+		var msie = ua.indexOf("MSIE ");
+		var isMobile = {Android: function() {return navigator.userAgent.match(/Android/i);},BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},Windows: function() {return navigator.userAgent.match(/IEMobile/i);},any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}};
+	function isIE() {
+		ua = navigator.userAgent;
+		var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+		return is_ie; 
+	}
+	if(isIE()){
+		$('body').addClass('ie');
+	}
+	if(isMobile.any()){
+		$('body').addClass('touch');
+	}
+$('.wrapper').addClass('loaded');/********* меню бургер  ****************/$('.header__burger').click(function(event) {	$(this).toggleClass('active');	$('.menu__body').toggleClass('active');	$('body').toggleClass('lock');});/**********\/меню бургер****************//* адаптив картинок с помощью jquery, обернуть img в div с классом ibg*/function bg(){	$.each($('.bg'), function(index, val) {		if($(this).find('img').length>0){			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');		}	});}bg();/***\/ibg***********************************************/var array = ['John', 'Martin'];
+});
